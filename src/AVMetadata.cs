e2fb9b068d44f6026982ebf4865997e7cdda7c43
ermau <me@ermau.com>
2010-10-30 18:28:00 -0400

    AVChapter

---
+++ src/AVMetadata.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace libavnet
+{
+	[StructLayout (LayoutKind.Sequential)]
+	#if FF_API_OLD_METADATA
+	public class AVMetadataConv
+	#else
+	public class AVMetadata
+	#endif
+	{
+	}
+}
