222228313fa47eed66d4fcdb10d94f436ae9f028
ermau <me@ermau.com>
2010-10-30 17:14:29 -0400

    Made AVOutputFormat delegates non-global

--- src/AVOutputFormat.cs
+++ src/AVOutputFormat.cs
@@ -3,15 +3,15 @@ using System.Runtime.InteropServices;
 
 namespace libavnet
 {
-	public delegate int write_header (ref AVFormatContext context);
-	public delegate int write_packet (ref AVFormatContext context, ref AVPacket pkt);
-	public delegate int write_trailer (ref AVFormatContext context);
-	public delegate int set_parameters (ref AVFormatContext context, ref AVFormatParameters parameters);
-	public delegate int interleave_packet (ref AVFormatContext context, ref AVPacket pout, AVPacket pin, int flush);
-
 	[StructLayout (LayoutKind.Sequential)]
 	public class AVOutputFormat
 	{
+		public delegate int write_header_delegate (ref AVFormatContext context);
+		public delegate int write_packet_delegate (ref AVFormatContext context, ref AVPacket pkt);
+		public delegate int write_trailer_delegate (ref AVFormatContext context);
+		public delegate int set_parameters_delegate (ref AVFormatContext context, ref AVFormatParameters parameters);
+		public delegate int interleave_packet_delegate (ref AVFormatContext context, ref AVPacket pout, AVPacket pin, int flush);
+
 		[MarshalAs(UnmanagedType.LPStr)]
 		public string name;
 		[MarshalAs(UnmanagedType.LPStr)]
@@ -25,14 +25,14 @@ namespace libavnet
 		public int audio_codec;
 		public int video_codec;
 
-		public write_header write_header;
-		public write_packet write_packet;
-		public write_trailer write_trailer;
+		public write_header_delegate write_header;
+		public write_packet_delegate write_packet;
+		public write_trailer_delegate write_trailer;
 
 		public int flags;
 
-		public set_parameters set_parameters;
-		public interleave_packet interleave_packet;
+		public set_parameters_delegate set_parameters;
+		public interleave_packet_delegate interleave_packet;
 
 		public IntPtr codec_tag;
 		public int subtitle_codec;
