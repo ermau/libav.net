3ec17464866a7eab33b6c66d609233bb1ec2e18b
ermau <me@ermau.com>
2010-10-30 17:24:19 -0400

    AVMetadataTag

---
+++ src/AVMetadataTag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace libavnet
+{
+	[StructLayout (LayoutKind.Sequential)]
+	public class AVMetadataTag
+	{
+		[MarshalAs (UnmanagedType.LPStr)]
+		public string key;
+		
+		[MarshalAs (UnmanagedType.LPStr)]
+		public string value;
+	}
+}
