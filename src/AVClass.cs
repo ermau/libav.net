771126992b1a79ea0021ac82c8ca2d55b6e8becf
ermau <me@ermau.com>
2010-10-30 18:22:53 -0400

    AVClass

--- src/AVClass.cs
+++ src/AVClass.cs
@@ -1,9 +1,20 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace libavnet
 {
 	[StructLayout (LayoutKind.Sequential)]
 	public class AVClass
 	{
+		[MarshalAs (UnmanagedType.LPStr)]
+		public string class_name;
+
+		public Func<string, IntPtr> item_name;
+
+		public IntPtr option;
+
+		public int version;
+		public int log_level_offset_offset;
+		public int parent_log_context_offset;
 	}
 }
--- src/Helpers.cs
+++ src/Helpers.cs
@@ -1,5 +1,8 @@
 ﻿namespace libavnet
 {
+	public delegate TResult Func<TResult>();
+	public delegate TResult Func<TResult, TArg1> (TArg1 arg1);
+	public delegate TResult Func<TResult, TArg1, TArg2> (TArg1 arg1, TArg2 arg2);
 	public delegate TResult Func<TResult, TArg1, TArg2, TArg3> (TArg1 arg1, TArg2 arg2, TArg3 arg3);
 	public delegate TResult Func<TResult, TArg1, TArg2, TArg3, TArg4> (TArg1 arg1, TArg2 arg2, TArg3 arg3, TArg4 arg4);
 }
