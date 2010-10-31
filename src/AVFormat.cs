98935bf843c248877a16a06a7c1acee19512dda6
ermau <me@ermau.com>
2010-10-30 23:57:15 -0400

    Trying to get things working

--- src/AVFormat.cs
+++ src/AVFormat.cs
@@ -10,19 +10,31 @@ namespace libavnet
 		[DllImport ("libavformat.dll", CallingConvention = CallingConvention.Cdecl)]
 		public static extern void av_register_all();
 
-		[DllImport ("libavformat.dll", EntryPoint = "av_guess_format", CallingConvention = CallingConvention.Cdecl)]
+		[DllImport ("libavformat.dll", CallingConvention = CallingConvention.Cdecl)]
 		public static extern AVOutputFormat av_guess_format (
 			[MarshalAs(UnmanagedType.LPStr)] string short_name,
 			[MarshalAs(UnmanagedType.LPStr)] string filename,
 			[MarshalAs(UnmanagedType.LPStr)] string mime_type);
 
-		public static int av_open_input_file (out AVFormatContext context, string filename)
-		{
-			return av_open_input_file (out context, filename, IntPtr.Zero, 0, IntPtr.Zero);
-		}
+		[DllImport ("libavformat.dll", CallingConvention = CallingConvention.Cdecl)]
+		public static extern int av_guess_codec (
+		    AVOutputFormat fmt,
+		    [MarshalAs (UnmanagedType.LPStr)] string short_name,
+		    [MarshalAs (UnmanagedType.LPStr)] string filename,
+		    [MarshalAs (UnmanagedType.LPStr)] string mime_type,
+		    AVMediaType type);
 
-		[DllImport("libavformat.dll", CallingConvention = CallingConvention.Cdecl)]
-		private static extern int av_open_input_file ([MarshalAs (UnmanagedType.LPStruct)] out AVFormatContext context,
+		//public static int av_open_input_file (out AVFormatContext context, string filename)
+		//{
+		//    IntPtr ctx_ptr;
+		//    int result = av_open_input_file (out ctx_ptr, filename, IntPtr.Zero, 0, IntPtr.Zero);
+
+		//    context = (AVFormatContext)Marshal.PtrToStructure (ctx_ptr, typeof (AVFormatContext));
+		//    return result;
+		//}
+
+		[DllImport("libavformat.dll")]
+		public static extern int av_open_input_file (out IntPtr context,
 		                                              [MarshalAs (UnmanagedType.LPStr)] string filename,
 		                                              IntPtr fmt,
 		                                              int buf_size,
@@ -32,8 +44,25 @@ namespace libavnet
 		public static extern int av_open_input_file (
 			[MarshalAs(UnmanagedType.LPStruct)] out AVFormatContext context,
 			[MarshalAs (UnmanagedType.LPStr)] string filename,
-			[MarshalAs(UnmanagedType.LPStruct)] ref AVInputFormat fmt,
+			[MarshalAs(UnmanagedType.LPStruct)] AVInputFormat fmt,
 			int buf_size,
-			[MarshalAs(UnmanagedType.LPStruct)] ref AVFormatParameters ap);
+			[MarshalAs(UnmanagedType.LPStruct)] AVFormatParameters ap);
+
+		[DllImport ("libavformat.dll")]
+		public static extern int av_find_stream_info (AVFormatContext ic);
+
+		[DllImport ("libavformat.dll")]
+		public static extern int av_find_stream_info (IntPtr ptr);
+	}
+
+	public enum AVMediaType
+	{
+		UNKNOWN = -1,
+		VIDEO = 0,
+		AUDIO = 1,
+		DATA = 2,
+		SUBTITLE = 3,
+		ATTACHMENT = 4,
+		NB = 5
 	}
 }
--- src/AVFormatContext.cs
+++ src/AVFormatContext.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿#define FF_API_OLD_METADATA
+#define FF_API_LAVF_UNUSED
+
+using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -6,91 +9,124 @@ using System.Text;
 namespace libavnet
 {
 	[StructLayout (LayoutKind.Sequential)]
-	public class AVFormatContext
+	public struct AVFormatContext
 	{
-		public IntPtr av_class;
-		public IntPtr iformat;
-		public IntPtr oformat;
-		private IntPtr priv_data;
-		public IntPtr pb;
-		public uint nb_streams;
-		public IntPtr streams;
-
-		[MarshalAs(UnmanagedType.LPStr, SizeConst = 1024)]
-		public string filename;
-
-		public long timestamp;
-		#if FF_API_OLD_METADATA
-		[MarshalAs(UnmanagedType.LPStr, SizeConst = 512)]
-		public string title;
-		[MarshalAs(UnmanagedType.LPStr, SizeConst = 512)]
-		public string author;
-		[MarshalAs(UnmanagedType.LPStr, SizeConst = 512)]
-		public string copyright;
-		[MarshalAs(UnmanagedType.LPStr, SizeConst = 512)]
-		public string comment;
-		[MarshalAs(UnmanagedType.LPStr, SizeConst = 512)]
-		public string album;
-		public int year;
-		public int track;
-		[MarshalAs(UnmanagedType.LPStr, SizeConst = 32)]
-		public string genre;
-		#endif
-
-		public int ctx_flags;
-		public IntPtr packet_buffer;
-		public long start_time;
-		public long duration;
-		public long file_size;
-		public int bit_rate;
-		public IntPtr cur_st;
-
-		#if FF_API_LAVF_UNUSED
-		public IntPtr cur_ptr_deprecated;
-		public int cur_len_deprecated;
-		public IntPtr cur_pkt_deprecated;
-		#endif
-
-		public long data_offset;
-		public int index_built;
-		public int mux_rate;
-		public uint packet_size;
-		public int preload;
-		public int max_delay;
-
-		public int loop_output;
-		public AVFMT_FLAGS flags;
-		public int loop_input;
-		public uint probesize;
-		public int max_analyze_duration;
+	    public IntPtr av_class;
+	    public IntPtr iformat;
+	    public IntPtr oformat;
+	    public IntPtr priv_data;
+	    public IntPtr pb;
 		
-		public IntPtr key;
-		public int keylen;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int nb_streams;
+	    public IntPtr streams;
 
-		public uint nb_programs;
-		public IntPtr programs;
+	    [MarshalAs (UnmanagedType.ByValTStr, SizeConst = 1024)]
+	    public string filename;
 
-		public int video_codec_id;
-		public int audio_codec_id;
-		public int subtitle_codec_id;
+	    [MarshalAs (UnmanagedType.I8)]
+	    public long timestamp;
+	    #if FF_API_OLD_METADATA
+	    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 512)]
+	    public string title;
+	    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 512)]
+	    public string author;
+	    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 512)]
+	    public string copyright;
+	    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 512)]
+	    public string comment;
+	    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 512)]
+	    public string album;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int year;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int track;
+	    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+	    public string genre;
+	    #endif
 
-		public uint max_index_size;
-		public uint max_picture_buffer;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int ctx_flags;
+	    public IntPtr packet_buffer;
+	    [MarshalAs (UnmanagedType.I8)]
+	    public long start_time;
+	    [MarshalAs (UnmanagedType.I8)]
+	    public long duration;
+	    [MarshalAs (UnmanagedType.I8)]
+	    public long file_size;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int bit_rate;
+	    public IntPtr cur_st;
+
+	    #if FF_API_LAVF_UNUSED
+	    public IntPtr cur_ptr_deprecated;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int cur_len_deprecated;
+	    public IntPtr cur_pkt_deprecated;
+	    #endif
+
+	    [MarshalAs (UnmanagedType.I8)]
+	    public long data_offset;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int index_built;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int mux_rate;
+	    [MarshalAs (UnmanagedType.U4)]
+	    public uint packet_size;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int preload;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int max_delay;
+
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int loop_output;
+	    public AVFMT_FLAGS flags;
+
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int loop_input;
+	    [MarshalAs (UnmanagedType.U4)]
+	    public uint probesize;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int max_analyze_duration;
 		
-		public uint nb_chapters;
-		public IntPtr chapters;
+	    public IntPtr key;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int keylen;
 
-		public int debug;
+	    [MarshalAs (UnmanagedType.U4)]
+	    public uint nb_programs;
+	    public IntPtr programs;
 
-		public IntPtr raw_packet_buffer;
-		public IntPtr raw_packet_buffer_end;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int video_codec_id;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int audio_codec_id;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int subtitle_codec_id;
+
+	    [MarshalAs (UnmanagedType.U4)]
+	    public uint max_index_size;
+	    [MarshalAs (UnmanagedType.U4)]
+	    public uint max_picture_buffer;
 		
-		public IntPtr packet_buffer_end;
+	    [MarshalAs (UnmanagedType.U4)]
+	    public uint nb_chapters;
+	    public IntPtr chapters;
 
-		public IntPtr metadata;
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int debug;
 
-		public int raw_packet_buffer_remaining_size;
-		public long start_time_realtime;
+	    public IntPtr raw_packet_buffer;
+	    public IntPtr raw_packet_buffer_end;
+		
+	    public IntPtr packet_buffer_end;
+
+	    public IntPtr metadata;
+
+	    [MarshalAs (UnmanagedType.I4)]
+	    public int raw_packet_buffer_remaining_size;
+	    [MarshalAs (UnmanagedType.I8)]
+	    public long start_time_realtime;
 	}
 
 	[Flags]
